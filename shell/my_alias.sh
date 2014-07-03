2c46392546f9d5729580dfadc18a7715645738d4
Shijie Jin <kingshijie@163.com>
2014-07-03 12:35:59 -0700

    123

--- shell/my_alias.sh
+++ shell/my_alias.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#navigate
 alias h='cd ~'
 alias dl='cd ~/Downloads'
 alias dc='cd ~/Documents'
 alias ll='ls -l'
 alias la='ls -a'
+
+#git
+alias gst='git status'
+alias gadd='git add'
+alias gcm='git commit -m '
+alias grm='git rm '
+alias gco='git checkout '
+alias gpush='git push '
